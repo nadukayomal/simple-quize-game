c5673a676bd956fe73155488a010f4b788a9b526
Naduka Yomal <118655544+nadukayomal@users.noreply.github.com>
2023-11-20 17:13:44 +0530

    updated the buttons

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
 <body>
@@ -44,16 +45,11 @@
                     </div>
                 </div>
             </div>
-            <div class="d-grid col-6 mx-auto">
-                <button type="submit" class="btn btn-primary">Register </button>
+            <div class="col-6 mx-auto">
+                <button type="submit" class="btn btn-lg">Register </button>
+                <!-- log in button should be change it's attributes later -->
+                <button type="submit" class="btn btn-lg">log in </button>
             </div>
-            
-            <!-- this is login button -->
-            <br>
-            <div>
-                <div class="btn btn-primary">log in</div>
-            <div class="d-grid col-2 mx-auto">
-            </div></div>
         </div>
     </form>
 </body>
