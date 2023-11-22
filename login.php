0341bb2df5c64b1abad6379fee2bd016706f8458
Naduka Yomal <118655544+nadukayomal@users.noreply.github.com>
2023-11-22 12:07:56 +0530

    create and modify Login page

--- login.php
+++ login.php
@@ -3,26 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="index.css">
+        <title>User login</title>
     </head>
     <body>
         <form method = "POST" autocomplete = "off" action="">
             <div class="container col-6">
-                <div class="fs-3 text-center">User Registration</div>
-                <div class="row">
-                    <div class="col">
-                        <div class="mb-3">
-                            <div class="form-label" for="f_name">First name : </div>
-                            <input class="form-control" type="text" id="f_name" name="f_name">
-                        </div>
-                    </div>
-                    <div class="col ">
-                        <div class="mb-3">
-                            <div class="form-label" for="l_name">Last name : </div>
-                            <input class="form-control" type="text" id="l_name" name="l_name">
-                        </div>
-                    </div>
-                </div>
+                <div class="fs-3 text-center">User Login</div>
                 <div class="row">
                     <div class="mb-3">
                         <div class="form-label" for="user_name">User name : </div>
@@ -30,23 +18,14 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col">
-                        <div class="mb-3">
-                            <div class="form-label">Create password : </div>
-                            <input type="password" class="form-control" id="create_passwd" name="create_passwd">
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="mb-3">
-                            <div class="form-label">Confirm password : </div>
-                            <input type="password" class="form-control" id="conf_passwd" name="conf_passwd">
-                        </div>
+                    <div class="mb-3">
+                        <div class="form-label" for="user_name">Password : </div>
+                        <input type="password" placeholder="type password here" class="form-control" id="user_password" name="user_password">
                     </div>
                 </div>
-                <div class="col-6 mx-auto">
-                    <button type="submit" class="btn btn-lg">Register </button>
-                    <!-- log in button should be change it's attributes later -->
-                    <button type="submit" class="btn btn-lg">log in </button>
+                <div class="d-flex justify-content-center">
+                    <!-- insert external css form index.css page  -->
+                    <button type="button" class="btn btn-lg login-btn">log in</button>
                 </div>
             </div>
         </form>
